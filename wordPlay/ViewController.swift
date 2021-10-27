5c636918c45a5fdc2b2afa8af2780f3f90d4a57a
Grant Spraker <grant.spraker@gmail.com>
2021-10-27 10:57:14 -0600

    got it to work. with out any gliches to threats

--- wordPlay/ViewController.swift
+++ wordPlay/ViewController.swift
@@ -5,15 +5,68 @@
 //  Created by Grant Spraker on 10/26/21.
 //
 
-import UIKit
+//MARK: Border for the Button
+// Button Corner Radius and Border code from StackOverflow.com
+@IBDesignable extension UIButton {
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
     }
 
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
 
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    // Outlets
+    @IBOutlet weak var lableOne: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        textField.resignFirstResponder()
+
+        
+//MARK: MVP1 / lable "uncle"
+        lableOne.text = "My uncle wants to go the ______."
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let nextViewController = segue.destination as! WordController
+    
+        nextViewController.place1 = " "
+    
+    }
 }
 
