aecbf59b13e057cb2d83cb4b868c4c4575c9f946
Grant Spraker <grant.spraker@gmail.com>
2021-10-29 09:57:10 -0600

    done with MVP & STRETCH 1. that's it.

--- wordPlay/WordController.swift
+++ wordPlay/WordController.swift
@@ -12,10 +12,6 @@ class WordController: UIViewController
     //Outlets
     @IBOutlet weak var labelTwo: UILabel!
     
-    //STRECT 2
-    @IBOutlet weak var sentLabel: UILabel!
-    @IBOutlet weak var wordTextField: UITextField!
-    
     //MARK: MVP & 1
     var place1 = " " // N
     //STRETCH 1
@@ -48,27 +44,4 @@ class WordController: UIViewController
             view.addSubview(imageView)
             self.view.sendSubviewToBack(imageView)
     }
-    
-//MARK: STRECT 2
-    // stackoverflow
-    let string = \(place3)
-    let string = \(place2)
-    let string = \(place1)
-    let characters = Array(string)
-    print(characters)
-  
-    
-    @IBAction func alertAlert(_ sender: Any)
-    {
-        //Create Alert
-        let alert = UIAlertController (title: "Good job 😀", message: "_____", preferredStyle: .alert)
-        
-        //Put OK Button on Alert
-        let ok = UIAlertAction (title: "OK", style: .default, handler: nil)
-        
-        alert.addAction(ok)
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    
 }
